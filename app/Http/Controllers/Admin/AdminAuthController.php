8ffaf058d9410e2682891892a420ac88e644b9a5
Himel-032 <139494430+Himel-032@users.noreply.github.com>
2025-08-09 15:06:14 +0600

    Admin profile update section done , name,email, profile photo, password change

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -41,6 +41,38 @@ class AdminAuthController extends Controller
     {
         return view('admin.profile');
     }
+    public function profile_submit(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+        ]);
+        $admin = Admin::where('id', Auth::guard('admin')->user()->id)->first();
+        //  photo section
+        if($request->photo){
+            $request->validate([
+                'photo' => ['mimes:jpg, jpeg, png, gif', 'max: 2024'],
+            ]);
+
+            $final_name = 'admin_'.time().'.'.$request->photo->extension();
+            $request->photo->move(public_path('uploads'), $final_name);
+            unlink(public_path('uploads/'.$admin->photo));
+            $admin->photo = $final_name;
+        }
+
+        if($request->password){
+            $request->validate([
+                'password' => ['required'],
+                'confirm_password' => ['required', 'same:password'],
+            ]);
+            $admin->password = Hash::make($request->password);
+        }
+        $admin->name = $request->name;
+        $admin->email = $request->email;
+        $admin->update();
+        return redirect()->back()->with('success', 'Profile is updated!');
+
+    }
     public function forget_password()
     {
         return view('admin.forget-password');
